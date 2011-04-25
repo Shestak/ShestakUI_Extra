cfa048ae91b0ce78bfe0ee95017e2482a512a2a4
Shestak <misha.shestakov@gmail.com>
2011-04-25 05:41:58 +0400

    Cleanup.

--- Modules/Misc/MoveBlizzard.lua
+++ Modules/Misc/MoveBlizzard.lua
@@ -46,11 +46,11 @@ for i, v in pairs({
 })
 do
 	if _G[v] then
-	_G[v]:EnableMouse(true)
-	_G[v]:SetMovable(true)
-	_G[v]:SetClampedToScreen(true)
-	_G[v]:RegisterForDrag("LeftButton")
-	_G[v]:SetScript("OnDragStart", function(self) self:StartMoving() end)
-	_G[v]:SetScript("OnDragStop", function(self) self:StopMovingOrSizing() end)
+		_G[v]:EnableMouse(true)
+		_G[v]:SetMovable(true)
+		_G[v]:SetClampedToScreen(true)
+		_G[v]:RegisterForDrag("LeftButton")
+		_G[v]:SetScript("OnDragStart", function(self) self:StartMoving() end)
+		_G[v]:SetScript("OnDragStop", function(self) self:StopMovingOrSizing() end)
 	end
 end

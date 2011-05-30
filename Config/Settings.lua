cccf4dbafcf744ee56c749008d219f51d97f3656
Shestak <misha.shestakov@gmail.com>
2011-05-30 17:35:01 +0400

    Skins options is disabled by default. Same cleanup.

--- Config/Settings.lua
+++ Config/Settings.lua
@@ -25,18 +25,18 @@ C["extra_general"] = {
 --	Extra ActionBar options
 ----------------------------------------------------------------------------------------
 C["extra_bar"] = {
-	["enable"] = false,							-- 
-	["button_size"] = 30,						-- 
-	["button_space"] = 3,						-- 
-	["vertical"] = false,						-- 
+	["enable"] = false,							-- Enable extra actionbar
+	["button_size"] = 30,						-- Buttons size
+	["button_space"] = 3,						-- Buttons space
+	["vertical"] = false,						-- Vertical orientation
 }
 
 ----------------------------------------------------------------------------------------
 --	Skins options (Maybe not work correctly!!!)
 ----------------------------------------------------------------------------------------
 C["extra_skins"] = {
-	["blizzard_frames"] = true,					-- Blizzard frames skin
-	["minimap_buttons"] = true,					-- Skin addons icons on minimap
+	["blizzard_frames"] = false,				-- Blizzard frames skin
+	["minimap_buttons"] = false,				-- Skin addons icons on minimap
 	["blood_shield_tracker"] = false,			-- BloodShieldTracker skin
 	["combustion_helper"] = false,				-- CombustionHelper skin
 	["roll_tracker"] = false,					-- RollTracker skin
@@ -45,15 +45,4 @@ C["extra_skins"] = {
 	["shield_monitor"] = false,					-- ShieldMonitor skin
 	["cool_line"] = false,						-- CoolLine skin
 	["mfclip"] = false,							-- MFClip skin
-}
-
-----------------------------------------------------------------------------------------
---	Position options
-----------------------------------------------------------------------------------------
-C["extra_position"] = {
-	["micro_menu"] = {"TOPLEFT", UIParent, "TOPLEFT", 2, -2},		-- Micro menu
-	["arena_calc"] = {"CENTER", UIParent, "CENTER", 0, 0},			-- Arena calculator
-	["switch_spec"] = {"CENTER", UIParent, "CENTER", 0, -200},		-- Talent spec panel
-	["mark_bar"] = {"CENTER", UIParent, "CENTER", 100, 50},			-- Mark bar
-	["extra_bar"] = {"BOTTOM", UIParent, "BOTTOM", 0, 100},			-- Extra bar
 }

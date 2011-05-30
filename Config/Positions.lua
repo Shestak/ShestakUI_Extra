fb23b6f029de8907c9f3c71bb577d251cb4ef57d
Shestak <misha.shestakov@gmail.com>
2011-05-30 17:35:30 +0400

    Added Positions.lua/Profiles.lua files.

---
+++ Config/Positions.lua
@@ -0,0 +1,12 @@
+﻿local T, C, L = unpack(ShestakUI)
+
+----------------------------------------------------------------------------------------
+--	Position options
+----------------------------------------------------------------------------------------
+C["extra_position"] = {
+	["micro_menu"] = {"TOPLEFT", UIParent, "TOPLEFT", 2, -2},		-- Micro menu
+	["arena_calc"] = {"CENTER", UIParent, "CENTER", 0, 0},			-- Arena calculator
+	["switch_spec"] = {"CENTER", UIParent, "CENTER", 0, -200},		-- Talent spec panel
+	["mark_bar"] = {"CENTER", UIParent, "CENTER", 100, 50},			-- Mark bar
+	["extra_bar"] = {"BOTTOM", UIParent, "BOTTOM", 0, 100},			-- Extra bar
+}
---
+++ Config/Profiles.lua
@@ -0,0 +1,48 @@
+﻿local T, C, L = unpack(ShestakUI)
+
+----------------------------------------------------------------------------------------
+--	ShestakUI_Extra personal configuration file
+--	BACKUP THIS FILE BEFORE UPDATING!
+--	ATTENTION: When saving changes to a file encoded file should be in UTF8
+----------------------------------------------------------------------------------------
+--	Configuration example:
+----------------------------------------------------------------------------------------
+-- if T.name == "MegaChar" then
+--		C["extra_general"].click_cast = true
+-- 		C["extra_general"].move_blizzard = false
+--		C["extra_bar"].enable = true
+--		C["extra_skins"].blizzard_frames = true
+--		C["extra_position"].micro_menu = {"TOPLEFT", UIParent, "TOPLEFT", 2, -2}
+-- end
+----------------------------------------------------------------------------------------
+
+----------------------------------------------------------------------------------------
+--	Per Class Config (overwrite general)
+--	Class need to be UPPERCASE
+----------------------------------------------------------------------------------------
+if T.class == "DRUID" then
+
+end
+
+----------------------------------------------------------------------------------------
+--	Per Character Name Config (overwrite general and class)
+--	Name need to be case sensitive
+----------------------------------------------------------------------------------------
+if T.name == "MegaChar" then
+
+end
+
+----------------------------------------------------------------------------------------
+--	Per Max Character Level Config (overwrite general, class and name)
+----------------------------------------------------------------------------------------
+if T.level ~= MAX_PLAYER_LEVEL then
+
+end
+
+----------------------------------------------------------------------------------------
+--	My Config
+----------------------------------------------------------------------------------------
+if T.author == true then
+	C["extra_skins"].blizzard_frames = true
+	C["extra_skins"].minimap_buttons = true
+end

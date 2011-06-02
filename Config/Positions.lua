6ad0e57e23a7f97482fd10e8f7d8c410d85e9017
Shestak <misha.shestakov@gmail.com>
2011-06-03 00:10:34 +0400

    Slightly changed ExtraBar default position.

--- Config/Positions.lua
+++ Config/Positions.lua
@@ -8,6 +8,6 @@ C["extra_position"] = {
 	["arena_calc"] = {"CENTER", UIParent, "CENTER", 0, 0},			-- Arena calculator
 	["switch_spec"] = {"CENTER", UIParent, "CENTER", 0, -200},		-- Talent spec panel
 	["mark_bar"] = {"CENTER", UIParent, "CENTER", 100, 50},			-- Mark bar
-	["extra_bar"] = {"BOTTOM", UIParent, "BOTTOM", 0, 100},			-- Extra bar
+	["extra_bar"] = {"BOTTOM", ActionBarAnchor, "TOP", 0, 3},		-- Extra bar
 	["archaeology"] = {"BOTTOMRIGHT", Minimap, "TOPRIGHT", 2, 5},	-- Archaeology frame
 }

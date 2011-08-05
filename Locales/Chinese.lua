2e3276a2cb3d5a69bc170a9a7efe239dbef668ee
tattoo <tat2dawn@live.cn>
2011-08-05 09:06:18 +0800

    Edited Locales/Chinese.lua via GitHub

--- Locales/Chinese.lua
+++ Locales/Chinese.lua
@@ -5,20 +5,20 @@ if T.client ~= "zhCN" then return end
 --	Localization for zhCN client
 ----------------------------------------------------------------------------------------
 -- Mark bar
-L_EXTRA_BUTTON_CLEAR = "Clear Target"
+L_EXTRA_BUTTON_CLEAR = "清除选定的目标"
 
 -- Announce fps
-L_EXTRA_ANNOUNCE_FPS = "My current FPS is "
+L_EXTRA_ANNOUNCE_FPS = "我目前的 FPS 是 "
 
 -- Click2cast
-L_EXTRA_BINDER_FRAMES = "Frames"
-L_EXTRA_BINDER_OPEN = "Open spell binder"
-L_EXTRA_BINDER_CLOSE = "Close spell binder"
+L_EXTRA_BINDER_FRAMES = "点击施法框体"
+L_EXTRA_BINDER_OPEN = "打开点击施法绑定页面"
+L_EXTRA_BINDER_CLOSE = "关闭点击施法绑定页面"
 
 -- BG announce
-L_EXTRA_ANNOUNCE_BG_LIGHT = "Light attack incoming to "
-L_EXTRA_ANNOUNCE_BG_MEDIUM = "Medium attack incoming to "
-L_EXTRA_ANNOUNCE_BG_HEAVY = "Heavy attack incoming to "
-L_EXTRA_ANNOUNCE_BG_CLEAR = "All clear at "
-L_EXTRA_ANNOUNCE_BG_FOCUS = "Focus Damage on "
-L_EXTRA_ANNOUNCE_BG_STEALTH = "Stealthed Enemies Nearby"
+L_EXTRA_ANNOUNCE_BG_LIGHT = "敌方小型攻击部队来到 "
+L_EXTRA_ANNOUNCE_BG_MEDIUM = "敌方中型攻击部队来到 "
+L_EXTRA_ANNOUNCE_BG_HEAVY = "敌方大部队来到 "
+L_EXTRA_ANNOUNCE_BG_CLEAR = " 这边已经清掉所有敌人"
+L_EXTRA_ANNOUNCE_BG_FOCUS = "集火目标 "
+L_EXTRA_ANNOUNCE_BG_STEALTH = "敌方潜行在我们旁边"

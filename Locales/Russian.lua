e92659962d2791f47b5a3ef32c601f6d9fbd3b74
Shestak <misha.shestakov@gmail.com>
2011-08-16 01:44:41 +0400

    Updated ruRU locale.

--- Locales/Russian.lua
+++ Locales/Russian.lua
@@ -8,17 +8,17 @@ if T.client ~= "ruRU" then return end
 L_EXTRA_BUTTON_CLEAR = "Отменить выбор"
 
 -- Announce fps
-L_EXTRA_ANNOUNCE_FPS = "My current FPS is "
+L_EXTRA_ANNOUNCE_FPS = "Кадров в секунду у меня "
 
 -- Click2cast
-L_EXTRA_BINDER_FRAMES = "Frames"
-L_EXTRA_BINDER_OPEN = "Open spell binder"
-L_EXTRA_BINDER_CLOSE = "Close spell binder"
+L_EXTRA_BINDER_FRAMES = "Рамки"
+L_EXTRA_BINDER_OPEN = "Назначения мыши"
+L_EXTRA_BINDER_CLOSE = "Назначения мыши"
 
 -- BG announce
-L_EXTRA_ANNOUNCE_BG_LIGHT = "Light attack incoming to "
-L_EXTRA_ANNOUNCE_BG_MEDIUM = "Medium attack incoming to "
-L_EXTRA_ANNOUNCE_BG_HEAVY = "Heavy attack incoming to "
-L_EXTRA_ANNOUNCE_BG_CLEAR = "All clear at "
-L_EXTRA_ANNOUNCE_BG_FOCUS = "Focus Damage on "
-L_EXTRA_ANNOUNCE_BG_STEALTH = "Stealthed Enemies Nearby"
+L_EXTRA_ANNOUNCE_BG_LIGHT = "Легкая атака на "
+L_EXTRA_ANNOUNCE_BG_MEDIUM = "Средняя атака на "
+L_EXTRA_ANNOUNCE_BG_HEAVY = "Тяжелая атака на "
+L_EXTRA_ANNOUNCE_BG_CLEAR = "Все чисто у "
+L_EXTRA_ANNOUNCE_BG_FOCUS = "Фокусная атака на "
+L_EXTRA_ANNOUNCE_BG_STEALTH = "Рядом враг в невидимости"

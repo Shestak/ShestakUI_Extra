4ea13c5b8f0b41f2ce9751a54fccf5b0ea93b23d
Shestak <misha.shestakov@gmail.com>
2011-09-04 02:35:58 +0400

    Same changes in TinyDPS skin.

--- Modules/Skins/TinyDPS.lua
+++ Modules/Skins/TinyDPS.lua
@@ -12,16 +12,18 @@ TDPSSkin:SetScript("OnEvent", function(self, event, addon)
 	tdps.width = 221
 	tdps.barHeight = 12
 	tdps.spacing = 3
+	tdps.border = {0, 0, 0, 0}
+	tdps.backdrop = {0, 0, 0, 0}
+	tdps.barbackdrop = {0, 0, 0, 0}
+	tdps.swapColor = true
+	tdps.showMinimapButton = false
+	tdps.layout = 11
 	tdpsVisibleBars = 7
 	tdpsFont.name = C.media.pixel_font
 	tdpsFont.size = C.media.pixel_font_size
 	tdpsFont.outline = C.media.pixel_font_style
 	tdpsFont.shadow = 0
 	tdpsTextOffset = 0
-	tdps.border = {0,0,0,0}
-	tdps.backdrop = {0,0,0,0}
-	tdps.barbackdrop = {0,0,0,0}
-	tdps.swapColor = true
 
 	if tdpsStatusBar then
 		tdpsStatusBar:SetStatusBarTexture(C.media.texture)

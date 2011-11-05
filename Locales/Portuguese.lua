1ca61a80be23e2488fdb6082e2c4c17b9e2f73ba
Shestak <misha.shestakov@gmail.com>
2011-11-05 06:48:44 +0400

    Added Portuguese locale for 4.3

---
+++ Locales/Portuguese.lua
@@ -0,0 +1,24 @@
+﻿local T, C, L = unpack(ShestakUI)
+if T.client ~= "ptBR" then return end
+
+----------------------------------------------------------------------------------------
+--	Localization for ptBR client(Need full translation)
+----------------------------------------------------------------------------------------
+-- Mark bar
+L_EXTRA_BUTTON_CLEAR = "Clear Target"
+
+-- Announce fps
+L_EXTRA_ANNOUNCE_FPS = "My current FPS is "
+
+-- Click2Cast
+L_EXTRA_BINDER_FRAMES = "Frames"
+L_EXTRA_BINDER_OPEN = "Open spell binder"
+L_EXTRA_BINDER_CLOSE = "Close spell binder"
+
+-- BG announce
+L_EXTRA_ANNOUNCE_BG_LIGHT = "Light attack incoming to "
+L_EXTRA_ANNOUNCE_BG_MEDIUM = "Medium attack incoming to "
+L_EXTRA_ANNOUNCE_BG_HEAVY = "Heavy attack incoming to "
+L_EXTRA_ANNOUNCE_BG_CLEAR = "All clear at "
+L_EXTRA_ANNOUNCE_BG_FOCUS = "Focus Damage on "
+L_EXTRA_ANNOUNCE_BG_STEALTH = "Stealthed Enemies Nearby"

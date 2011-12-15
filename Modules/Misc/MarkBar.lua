b1c0ebfb4adaa22359bc591c4eed5a5f46facd2f
Shestak <misha.shestakov@gmail.com>
2011-12-15 16:18:44 +0400

    Improved RaidMark panel.

--- Modules/Misc/MarkBar.lua
+++ Modules/Misc/MarkBar.lua
@@ -7,13 +7,13 @@ if C.extra_general.mark_bar ~= true then return end
 local button_size = 30
 
 local MarkBarBG = CreateFrame("Frame", "MarkBarAnchor", UIParent)
-MarkBarBG:CreatePanel("Invisible", (button_size * 4) + 9, (button_size * 3) - 4, unpack(C.extra_position.mark_bar))
+MarkBarBG:CreatePanel("Invisible", (button_size * 6) + 15, (button_size * 2) + 3, unpack(C.extra_position.mark_bar))
 MarkBarBG:SetFrameLevel(0)
 tinsert(T.MoverFrames, MarkBarAnchor)
 
 local icon = CreateFrame("Button", "Icon", MarkBarBG)
 local mark = CreateFrame("Button", "Menu", MarkBarBG)
-for i = 1, 8 do
+for i = 0, 8 do
 	mark[i] = CreateFrame("Button", "mark"..i, MarkBarBG)
 	mark[i]:CreatePanel("Transparent", button_size, button_size, "LEFT", MarkBarBG, "LEFT", 0, 0)
 	if i == 1 then
@@ -23,18 +23,23 @@ for i = 1, 8 do
 	else
 		mark[i]:SetPoint("LEFT", mark[i-1], "RIGHT", 3, 0)
 	end
+	mark[0]:SetPoint("TOPLEFT", mark[4], "TOPRIGHT", 3, 0)
 	mark[i]:EnableMouse(true)
 	mark[i]:SetScript("OnEnter", T.SetModifiedBackdrop)
 	mark[i]:SetScript("OnLeave", T.SetOriginalBackdrop)
 	mark[i]:SetScript("OnMouseUp", function() SetRaidTarget("target", i) end)
 
 	icon[i] = CreateFrame("Button", "icon"..i, MarkBarBG)
-	icon[i]:SetNormalTexture("Interface\\TargetingFrame\\UI-RaidTargetingIcons")
+	if i == 0 then
+		icon[i]:SetNormalTexture("Interface\\Buttons\\UI-GroupLoot-Pass-Up")
+	else
+		icon[i]:SetNormalTexture("Interface\\TargetingFrame\\UI-RaidTargetingIcons")
+	end
 	icon[i]:Size(button_size - 5, button_size - 5)
 	icon[i]:Point("CENTER", mark[i])
 
 	-- Set up each button
-	if i == 1 then 
+	if i == 1 then
 		icon[i]:GetNormalTexture():SetTexCoord(0, 0.25, 0, 0.25)
 	elseif i == 2 then
 		icon[i]:GetNormalTexture():SetTexCoord(0.25, 0.5, 0, 0.25)
@@ -53,32 +58,49 @@ for i = 1, 8 do
 	end
 end
 
--- Create Button for clear target
-local ClearTargetButton = CreateFrame("Button", "ClearTargetButton", MarkBarAnchor)
-ClearTargetButton:CreatePanel("Transparent", (button_size * 3) + 6, 20, "TOPLEFT", mark[5], "BOTTOMLEFT", 0, -3)
-ClearTargetButton:SetScript("OnEnter", T.SetModifiedBackdrop)
-ClearTargetButton:SetScript("OnLeave", T.SetOriginalBackdrop)
-ClearTargetButton:SetScript("OnMouseUp", function() SetRaidTarget("target", 0) end)
-ClearTargetButton:SetFrameStrata("HIGH")
+-- Create ReadyCheck Button
+local ReadyCheckButton = CreateFrame("Button", "ReadyCheckButton", MarkBarAnchor)
+ReadyCheckButton:CreatePanel("Transparent", button_size, button_size, "TOPLEFT", mark[8], "TOPRIGHT", 3, 0)
+ReadyCheckButton:SetScript("OnEnter", T.SetModifiedBackdrop)
+ReadyCheckButton:SetScript("OnLeave", T.SetOriginalBackdrop)
+ReadyCheckButton:SetScript("OnMouseUp", function() DoReadyCheck() end)
 
-ClearTargetButtonText = T.SetFontString(ClearTargetButton, C.media.pixel_font, C.media.pixel_font_size, C.media.pixel_font_style)
-ClearTargetButtonText:SetText(L_EXTRA_BUTTON_CLEAR)
-ClearTargetButtonText:SetPoint("CENTER")
-ClearTargetButtonText:Width(ClearTargetButton:GetWidth() - 4)
+local ReadyCheckButtonTexture = ReadyCheckButton:CreateTexture(nil, "OVERLAY")
+ReadyCheckButtonTexture:SetTexture(READY_CHECK_READY_TEXTURE)
+ReadyCheckButtonTexture:SetAllPoints(ReadyCheckButton)
 
--- Pull Button for clear target
+-- Create WorldMark Button
+local WorldMarkButton = CreateFrame("Button", "WorldMarkButton", MarkBarAnchor, "SecureActionButtonTemplate")
+WorldMarkButton:CreatePanel("Transparent", button_size, button_size, "TOPLEFT", mark[0], "TOPRIGHT", 3, 0)
+WorldMarkButton:SetScript("OnEnter", T.SetModifiedBackdrop)
+WorldMarkButton:SetScript("OnLeave", T.SetOriginalBackdrop)
+WorldMarkButton:SetAttribute("type1", "macro")
+WorldMarkButton:SetAttribute("macrotext1", "/click CompactRaidFrameManagerDisplayFrameLeaderOptionsRaidWorldMarkerButton")
+WorldMarkButton:HookScript("OnClick", function(self)
+	DropDownList1:ClearAllPoints()
+	DropDownList1:Point("TOPRIGHT", self, "BOTTOMRIGHT", 2, -4)
+end)
+
+local WorldMarkButtonTexture = WorldMarkButton:CreateTexture(nil, "OVERLAY")
+WorldMarkButtonTexture:SetTexture("Interface\\RaidFrame\\Raid-WorldPing")
+WorldMarkButtonTexture:Point("CENTER", 0, 0)
+WorldMarkButtonTexture:SetHeight(23)
+WorldMarkButtonTexture:SetWidth(23)
+
+-- Create Pull Button
 local PullTargetButton = CreateFrame("Frame", "PullTargetButton", MarkBarAnchor)
-PullTargetButton:CreatePanel("Transparent", button_size, 20, "BOTTOMLEFT", ClearTargetButton, "BOTTOMRIGHT", 3, 0)
+PullTargetButton:CreatePanel("Transparent", button_size, button_size, "TOP", WorldMarkButton, "BOTTOM", 0, -3)
 PullTargetButton:SetScript("OnEnter", T.SetModifiedBackdrop)
 PullTargetButton:SetScript("OnLeave", T.SetOriginalBackdrop)
 PullTargetButton:SetScript("OnMouseUp", function() PullCountdown.Pull(3) end)
-PullTargetButton:SetFrameStrata("HIGH")
 
-PullTargetButtonText = T.SetFontString(PullTargetButton, C.media.pixel_font, C.media.pixel_font_size, C.media.pixel_font_style)
-PullTargetButtonText:SetText("PULL")
-PullTargetButtonText:SetPoint("CENTER", 1, 0)
+local PullTargetButtonTexture = PullTargetButton:CreateTexture(nil, "OVERLAY")
+PullTargetButtonTexture:SetTexture("Interface\\Icons\\Ability_Hunter_Beastcall")
+PullTargetButtonTexture:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+PullTargetButtonTexture:Point("TOPLEFT", PullTargetButton, 2, -2)
+PullTargetButtonTexture:Point("BOTTOMRIGHT", PullTargetButton, -2, 2)
 
--- Check if we are Raid Leader or Raid Officer / Party
+-- Check if we are Raid Leader/Officer or in Party
 local function CheckRaidStatus()
 	local inInstance, instanceType = IsInInstance()
 	local partyMembers = GetNumPartyMembers()

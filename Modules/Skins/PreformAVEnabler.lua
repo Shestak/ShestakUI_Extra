08b92b7c396dbfdfefb4a5068ddcd356ce75c045
Shestak <misha.shestakov@gmail.com>
2012-02-01 19:58:42 +0400

    Added PreformAVEnabler skin.

--- Modules/Skins/PreformAVEnabler.lua
+++ Modules/Skins/PreformAVEnabler.lua
@@ -2,10 +2,20 @@
 if C.extra_skins.preform_av ~= true then return end
 
 ----------------------------------------------------------------------------------------
---	PreformAVEnabler skin(Not Ready)
+--	PreformAVEnabler skin
 ----------------------------------------------------------------------------------------
 local SkinPAV = CreateFrame("Frame")
 SkinPAV:RegisterEvent("PLAYER_ENTERING_WORLD")
 SkinPAV:SetScript("OnEvent", function(self, event)
-	
+	if not IsAddOnLoaded("PreformAVEnabler") then return end
+
+	PreformAVEnablerFrame:StripTextures()
+	PreformAVEnablerFrame:SetClampedToScreen(true)
+	PreformAVEnablerFrame:SetTemplate("Transparent")
+
+	T.SkinCheckBox(PreformAVEnablerButtonizeCheckButton)
+
+	PreformAVEnablerPVPBGButton:SkinButton()
+	PreformAVEnablerFrameCloseButton:SkinButton()
+	PreformAVEnablerFrameStatusButton:SkinButton()
 end)

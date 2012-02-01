1b4022ec580e8294b3f572ad731505b536e0c1ea
Shestak <misha.shestakov@gmail.com>
2012-02-01 16:19:33 +0400

    Fixed FS skin.

--- Modules/Skins/FaceShooter.lua
+++ Modules/Skins/FaceShooter.lua
@@ -9,11 +9,6 @@ FSSkin:RegisterEvent("PLAYER_LOGIN")
 FSSkin:SetScript("OnEvent", function(self, event, addon)
 	if not IsAddOnLoaded("FaceShooter") then return end
 
-	button1Mover.str:SetFont(C.font.stylization_font, C.font.stylization_font_size, C.font.stylization_font_style)
-	button1Mover.str:SetShadowOffset(C.font.stylization_font_shadow and 1 or 0, C.font.stylization_font_shadow and -1 or 0)
-	button2Mover.str:SetFont(C.font.stylization_font, C.font.stylization_font_size, C.font.stylization_font_style)
-	button2Mover.str:SetShadowOffset(C.font.stylization_font_shadow and 1 or 0, C.font.stylization_font_shadow and -1 or 0)
-
 	if ttlMover then
 		ttlMover.str:SetFont(C.font.stylization_font, C.font.stylization_font_size, C.font.stylization_font_style)
 		ttlMover.str:SetShadowOffset(C.font.stylization_font_shadow and 1 or 0, C.font.stylization_font_shadow and -1 or 0)
@@ -21,22 +16,31 @@ FSSkin:SetScript("OnEvent", function(self, event, addon)
 		ttlFrame.str:SetShadowOffset(C.font.stylization_font_shadow and 1 or 0, C.font.stylization_font_shadow and -1 or 0)
 	end
 
-	button1Button.texture:SetTexCoord(0.1, 0.9, 0.1, 0.9)
-	button2Button.texture:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+	button1Mover.str:SetFont(C.font.stylization_font, C.font.stylization_font_size, C.font.stylization_font_style)
+	button1Mover.str:SetShadowOffset(C.font.stylization_font_shadow and 1 or 0, C.font.stylization_font_shadow and -1 or 0)
 
-	if not button1Button.texture.b then
-		button1Button.texture.b = CreateFrame("Frame", nil, button1Button)
-		button1Button.texture.b:SetTemplate("Default")
-		button1Button.texture.b:SetFrameStrata("BACKGROUND")
-		button1Button.texture.b:Point("TOPLEFT", button1Button.texture, "TOPLEFT", -2, 2)
-		button1Button.texture.b:Point("BOTTOMRIGHT", button1Button.texture, "BOTTOMRIGHT", 2, -2)
+	button1Button.texture:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+
+	if not button1Button.b then
+		button1Button.b = CreateFrame("Frame", nil, button1Button)
+		button1Button.b:SetTemplate("Default")
+		button1Button.b:SetFrameStrata("BACKGROUND")
+		button1Button.b:Point("TOPLEFT", button1Button, "TOPLEFT", -2, 2)
+		button1Button.b:Point("BOTTOMRIGHT", button1Button, "BOTTOMRIGHT", 2, -2)
 	end
 
-	if not button2Button.texture.b then
-		button2Button.texture.b = CreateFrame("Frame", nil, button1Button)
-		button2Button.texture.b:SetTemplate("Default")
-		button2Button.texture.b:SetFrameStrata("BACKGROUND")
-		button2Button.texture.b:Point("TOPLEFT", button2Button.texture, "TOPLEFT", -2, 2)
-		button2Button.texture.b:Point("BOTTOMRIGHT", button2Button.texture, "BOTTOMRIGHT", 2, -2)
+	if button2Mover then
+		button2Mover.str:SetFont(C.font.stylization_font, C.font.stylization_font_size, C.font.stylization_font_style)
+		button2Mover.str:SetShadowOffset(C.font.stylization_font_shadow and 1 or 0, C.font.stylization_font_shadow and -1 or 0)
+
+		button2Button.texture:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+
+		if not button2Button.b then
+			button2Button.b = CreateFrame("Frame", nil, button2Button)
+			button2Button.b:SetTemplate("Default")
+			button2Button.b:SetFrameStrata("BACKGROUND")
+			button2Button.b:Point("TOPLEFT", button2Button, "TOPLEFT", -2, 2)
+			button2Button.b:Point("BOTTOMRIGHT", button2Button, "BOTTOMRIGHT", 2, -2)
+		end
 	end
 end)

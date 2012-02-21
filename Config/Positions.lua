87d9d7dc3d2c9dd12a15d79962a28f42ddc52f48
Shestak <misha.shestakov@gmail.com>
2012-02-21 08:48:50 +0400

    Added tank_announce position in Positions.lua file.

--- Config/Positions.lua
+++ Config/Positions.lua
@@ -11,4 +11,5 @@ C["extra_position"] = {
 	["extra_bar"] = {"BOTTOM", ActionBarAnchor, "TOP", 0, 3},		-- Extra bar
 	["archaeology"] = {"BOTTOMRIGHT", Minimap, "TOPRIGHT", 2, 5},	-- Archaeology frame
 	["bg_announce"] = {"CENTER", UIParent, "CENTER", 0, 0},			-- BG announce panel
+	["tank_announce"] = {"BOTTOMRIGHT", Minimap, "TOPRIGHT", 2, 5},	-- Tank announce panel
 }

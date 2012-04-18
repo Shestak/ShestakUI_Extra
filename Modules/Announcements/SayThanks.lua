b056bf244027c3953da7fca2cc218a870bc28de0
Shestak <misha.shestakov@gmail.com>
2012-04-18 18:39:45 +0400

    Do not report if the spell is cast on yourself(need test).

--- Modules/Announcements/SayThanks.lua
+++ Modules/Announcements/SayThanks.lua
@@ -10,12 +10,13 @@ local ST_Buffs = {
 	[1] = 85767,	-- Dark Intent
 	[2] = 54646,	-- Focus Magic
 	[3] = 29166,	-- Innervate
+	--[4] = 10060,	-- Power Infusion
 }
 
 SayThanks:SetScript("OnEvent", function(...)
 	local args = {...}
 	for i, v in ipairs(ST_Buffs) do
-		if args[14] == v and args[11] == T.name and (args[4] == "SPELL_AURA_APPLIED" or args[4] == "SPELL_AURA_REFRESH") then
+		if args[14] == v and args[11] == T.name and args[7] ~= T.name and (args[4] == "SPELL_AURA_APPLIED" or args[4] == "SPELL_AURA_REFRESH") then
 			SendChatMessage(L_EXTRA_ANNOUNCE_SS_THANKS..GetSpellLink(args[14])..", "..args[7], "WHISPER", nil, args[7])
 			DEFAULT_CHAT_FRAME:AddMessage(GetSpellLink(args[14])..L_EXTRA_ANNOUNCE_SS_RECEIVED..(args[7] or UNKNOWN))
 		end

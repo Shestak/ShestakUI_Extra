8ae30c27112a353d90973ad6ad1c96bd40ce47c2
Shestak <misha.shestakov@gmail.com>
2012-09-20 08:40:44 +0400

    Cleanup Archaeology and BadTank files.

--- Config/Positions.lua
+++ Config/Positions.lua
@@ -10,5 +10,5 @@ C["extra_position"] = {
 	["extra_bar"] = {"BOTTOM", ActionBarAnchor, "TOP", 0, 3},		-- Extra bar
 	["archaeology"] = {"BOTTOMRIGHT", Minimap, "TOPRIGHT", 2, 5},	-- Archaeology frame
 	["bg_announce"] = {"CENTER", UIParent, "CENTER", 0, 0},			-- BG announce panel
-	["tank_announce"] = {"BOTTOMRIGHT", Minimap, "TOPRIGHT", 2, 5},	-- Tank announce panel
+	["tank_announce"] = {"BOTTOMRIGHT", Minimap, "TOPRIGHT", 2, 4},	-- Tank announce panel
 }

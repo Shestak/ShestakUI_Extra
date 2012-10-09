70aa4378f035c275db7558accc0ef4c3ddcb4114
Shestak <misha.shestakov@gmail.com>
2012-10-09 15:47:08 +0400

    Cleanup.

--- Config/Positions.lua
+++ Config/Positions.lua
@@ -8,7 +8,6 @@ C["extra_position"] = {
 	["switch_spec"] = {"CENTER", UIParent, "CENTER", 0, -200},		-- Talent spec panel
 	["mark_bar"] = {"CENTER", UIParent, "CENTER", 100, 50},			-- Mark bar
 	["extra_bar"] = {"BOTTOM", ActionBarAnchor, "TOP", 0, 3},		-- Extra bar
-	["archaeology"] = {"BOTTOMRIGHT", Minimap, "TOPRIGHT", 2, 5},	-- Archaeology frame
 	["bg_announce"] = {"CENTER", UIParent, "CENTER", 0, 0},			-- BG announce panel
 	["tank_announce"] = {"BOTTOMRIGHT", Minimap, "TOPRIGHT", 2, 4},	-- Tank announce panel
 }
